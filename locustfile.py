6ced36eefa6fced4226b5a0a733bb47b14a91f93
Chao Peter Yang <cpyang@umich.edu>
2024-12-13 06:30:15 +0000

    I was dumb

---
+++ locustfile.py
@@ -0,0 +1,20 @@
+from locust import HttpUser, task, constant
+import random
+from pyquery import PyQuery as pq
+
+class ExampleFlaskAppUser(HttpUser):
+    wait_time = constant(2)
+
+    @task
+    def homepage(self):
+        self.client.get("/")
+
+    @task
+    def article_check(self):
+        res = self.client.get("/")
+        page_content = pq(res.content)
+
+        post_headings = page_content("a.article-title")
+
+        chosen_post = random.choice(post_headings)
+        self.client.get(chosen_post.attrib["href"])
