f83d4356baabb687276c7f2221f815097e48cc7b
Chao Peter Yang <cpyang@umich.edu>
2024-12-13 06:22:08 +0000

    changed secret stuff

--- smaller_inference_LLM_model.py
+++ smaller_inference_LLM_model.py
@@ -1,13 +1,43 @@
+import boto3
+from botocore.exceptions import ClientError
 import openai
 import json
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
+def get_secret():
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
+    secret_name = "openapikey"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+
+    return secret
+
+
+# openai.api_key = os.getenv('OPENAI_API_KEY')
+
+openai.api_key = get_secret()
+
+
 instruction = (
     "You are an experienced medical doctor. "
     "Provide a detailed and compassionate treatment plan to the patient's description "
